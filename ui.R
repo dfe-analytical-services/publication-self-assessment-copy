75712ee6322503bf277523d5370dd4c27cbc797f
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-12-21 12:29:22 +0000

    Testing again now the password has been updated

--- ui.R
+++ ui.R
@@ -15,8 +15,6 @@ fluidPage(
                         column(7,
                                paste0("Current environment = ", environment),
                                br(),
-                               paste0("Config environment = ", config$uid),
-                               br(),
                                h3("What is this app for?"),
                                "This app allows you to assess different aspects of your Official Statistics production processes against best practice.",
                                br(),
