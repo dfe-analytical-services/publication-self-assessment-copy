28e213b03621d4542cda60ed5a42c4b2f3df4c25
Cam Race <cameron.race@education.gov.uk>
2021-11-08 17:45:07 +0000

    commit cam's local faffery

--- manualDB.R
+++ manualDB.R
@@ -26,13 +26,46 @@ con <- DBI::dbConnect(odbc::odbc(),
 
 # KPIs ======================================================================================================================================
 
+# Publication responses
+
+nonResponders <- prodData %>%
+  dplyr::mutate(date = as.character(date)) %>% 
+  dplyr::filter(date == "2019-09-28 00:00:00") %>% # View()
+  dplyr::filter(!publication %in% pubsToRemove$publication) %>% 
+  dplyr::distinct(publication)
+
+message("Out of ", totalPubs, " publications, ", nrow(nonResponders), " are yet to respond. These are: ")
+print(nonResponders)
+
+# Below here is a mess of things, I (Cam) will review and tidy to make more useful...
+
 stop("Do not source or highlight all of this script. Run lines individually.")
 
 prodData <- dplyr::tbl(con, dbplyr::in_schema("dbo","publicationTrackingProduction")) %>%
   dplyr::collect()
 
+updated_master <- read.csv("08112021_masterList.csv")
+
+upMasterPubOnly <- updated_master %>% 
+  filter(rap == "Yes") %>% 
+  distinct(publication)
+
+upMasterPubRemove <- updated_master %>% 
+  filter(rap == "No") %>% 
+  distinct(publication)
+
+
+pubsToRemove <- prodData %>% 
+  dplyr::distinct(publication) %>% 
+  dplyr::filter(publication %in% upMasterPubRemove$publication)
+
+pubsToRemove <- rbind(pubsToRemove, "Multi-academy trust performance measures at key stage 2")
+  
+
 totalPubs <- prodData %>% 
   dplyr::distinct(publication) %>% 
+  #dplyr::filter(!publication %in% extraneousPublications$publication) %>% 
+  dplyr::filter(!publication %in% pubsToRemove$publication) %>% 
   nrow()
 
 # On EES
@@ -52,6 +85,10 @@ onEESactually <- onFindStats[onFindStats %>% stringr::str_detect(".*Create your 
 # Good and great practice
   
 goodGreatPubs <- prodData %>% 
+  group_by(publication) %>%
+  filter(date < "2021-10-31") %>% 
+  slice(which.max(date)) %>% 
+  ungroup() %>% 
   dplyr::filter(
     processing_with_code == "Yes",
     sensible_folder_file_structure == "Yes",
@@ -67,19 +104,84 @@ goodGreatPubs <- prodData %>%
     peer_review_within_team == "Yes",
     publication_specifc_automated_qa == "Yes"
     ) %>% 
+  dplyr::filter(!publication %in% pubsToRemove$publication) %>% 
   dplyr::distinct(publication)
 
-# Publication responses
 
-nonResponders <- prodData %>%
-  dplyr::mutate(date = as.character(date)) %>% 
-  dplyr::filter(date == "2019-09-28 00:00:00") %>% # View()
+goodPubs <- prodData %>% 
+  group_by(publication) %>%
+  slice(which.max(date)) %>% 
+  ungroup() %>% 
+  dplyr::filter(
+    processing_with_code == "Yes",
+    sensible_folder_file_structure == "Yes",
+    approporiate_tools == "Yes",
+    single_database == "Yes",
+    documentation == "Yes",
+    files_meet_data_standards == "Yes",
+    basic_automated_qa == "Yes"
+  ) %>% 
+  dplyr::filter(!publication %in% extraneousPublications) %>% 
   dplyr::distinct(publication)
 
-message("Out of ", totalPubs, " publications, ", nrow(nonResponders), " are yet to respond. These are: ")
-print(nonResponders)
+# Overall percentage
+
+goodgreat_steps <- prodData %>% 
+  group_by(publication) %>%
+  slice(which.max(date)) %>% 
+  ungroup() %>% 
+  dplyr::filter(!publication %in% pubsToRemove$publication) %>% 
+  select(processing_with_code,
+         sensible_folder_file_structure,
+         approporiate_tools,
+         single_database,
+         documentation,
+         files_meet_data_standards,
+         basic_automated_qa,
+         recyclable_code,
+         single_data_production_scripts,
+         final_code_in_repo,
+         automated_insight_summaries,
+         peer_review_within_team,
+         publication_specifc_automated_qa) %>% 
+  unlist()
+
+yes_steps <- length(goodgreat_steps[goodgreat_steps == "Yes"])
+  
+overall_percentage <- round(100 * yes_steps / length(goodgreat_steps), 2)
+
+# Overall percentage
+
+goodgreat_steps <- prodData %>% 
+  group_by(publication) %>%
+  filter(date < "2021-09-30") %>% 
+  slice(which.max(date)) %>% 
+  ungroup() %>% 
+  dplyr::filter(!publication %in% pubsToRemove$publication) %>% 
+  select(processing_with_code,
+         sensible_folder_file_structure,
+         approporiate_tools,
+         single_database,
+         documentation,
+         files_meet_data_standards,
+         basic_automated_qa,
+         recyclable_code,
+         single_data_production_scripts,
+         final_code_in_repo,
+         automated_insight_summaries,
+         peer_review_within_team,
+         publication_specifc_automated_qa) %>% 
+  unlist()
+
+yes_steps <- length(goodgreat_steps[goodgreat_steps == "Yes"])
+
+overall_percentage <- round(100 * yes_steps / length(goodgreat_steps), 2)
 
 # CSSU KPIs -------------------------------------------------------------------------------------------------------------------------------
+
+# overall % to target
+
+message("Progress: ", overall_percentage, "% towards great practice as a whole.")
 
 # On EES
 message("Progress: ", length(onEESactually) - 1, " out of ", totalPubs, " publications are on EES.") # Using -1 for now as there's one supplementary publication that isn't really a publication - though we could include for ease?
@@ -97,7 +199,7 @@ stop("Do not source or highlight all of this script. Run lines individually.")
 # Rudimentary QA of the publications in the SA app compared to the master list
 
 masterPublicationOnly <- dplyr::tbl(con, dbplyr::in_schema("dbo","publicationMasterList")) %>% 
-  dplyr::collect() %>%
+  dplyr::collect() %>% # write.csv("08112021_masterList.csv", row.names = FALSE)
   dplyr::filter(where == "Find stats") %>%
   dplyr::distinct(publication)
 
@@ -234,6 +336,11 @@ apply(publications_to_create, 1, createInSQL)
 # Delete publications ===========================================================================================================================================
 
 stop("Do not source or highlight all of this script. Run lines individually.")
+
+
+publications_to_delete <- fread("08112021_masterlist.csv") %>% 
+  filter(rap == "No") %>% 
+  pull(publication)
 
 publications_to_delete <- c("Destinations of key stage 4 and key stage 5 pupils", "Graduate Outcomes (LEO): all publications")
 
